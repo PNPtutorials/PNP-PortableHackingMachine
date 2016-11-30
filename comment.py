ccb6e1147e5b655762b50ce5fc3c814a92de4f43
PNPtutorials <parvinderyadav95@gmail.com>
2016-11-30 07:56:28 +0000

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,11 @@
+f=open("/root/.bashrc","r")
+lines=f.readlines()
+f.close()
+f=open("/root/.bashrc","w")
+for line in lines:
+	if line=="./ap.sh PNP pnptutorials &"+"\n":
+		f.write("#./ap.sh PNP pnptutorials &"+"\n")
+	else:
+		f.write(line)
+
+f.close()
